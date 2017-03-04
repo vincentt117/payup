88f55b9743e4fa061c3f83b48209e7343aae306b
Michael Kuang <Michael Kuang>
2017-03-04 14:15:00 -0500

    Added two buttons

--- app/PayUp/app/src/main/java/com/cuhacking/payup/SelectActivity.java
+++ app/PayUp/app/src/main/java/com/cuhacking/payup/SelectActivity.java
@@ -2,6 +2,9 @@ package com.cuhacking.payup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class SelectActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class SelectActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select);
+       // Button takePicture = (Button)findViewById(R.id.take_receipt_picture);
+       // Button viewBill = (Button)findViewById(R.id.view_bill);
+    }
+
+
+    public void takePicture(View v){
+        //TODO go to vincent
+        Log.d("TAG", "takePicture: Clicked");
+
+    }
+    public void viewBill(View v){
+        //TODO go to vincent
+        Log.d("TAG", "viewBill: Clicked");
     }
 }
