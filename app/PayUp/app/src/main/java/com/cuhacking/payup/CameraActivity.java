32a0f0716c788a435387e9d5d41495b35e71c3f9
Vincent Teng <vincent@dhcp-195-175.eduroam.carleton.ca>
2017-03-04 14:38:00 -0500

    Added camera activity

--- app/PayUp/app/src/main/java/com/cuhacking/payup/CameraActivity.java
+++ app/PayUp/app/src/main/java/com/cuhacking/payup/CameraActivity.java
@@ -1,8 +1,20 @@
 package com.cuhacking.payup;
 
-/**
- * Created by vincent on 2017-03-04.
- */
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class CameraActivity {
+public class CameraActivity extends AppCompatActivity {
+    // Assume thisActivity is the current activity
+    int permissionCheck = ContextCompat.checkSelfPermission(CameraActivity.this,
+            Manifest.permission.WRITE_CALENDAR);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+    }
 }
